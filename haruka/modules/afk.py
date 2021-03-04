0f472736f80e824c83ba11ef6935c37e2dabffd1
ichn666shit <75955944+ichn666shit@users.noreply.github.com>
2021-03-04 15:28:43 +0700

    Update afk.py

--- haruka/modules/afk.py
+++ haruka/modules/afk.py
@@ -42,7 +42,7 @@ def no_longer_afk(bot: Bot, update: Update):
     if res:
         firstname = update.effective_user.first_name
         try:
-            update.effective_message.reply_text(tld(chat.id, f"{firstname} is no longer AFK!"))
+            update.effective_message.reply_text(tld(chat.id, f"{firstname} udah afknya? lancar ga?"))
         except:
             return
 
